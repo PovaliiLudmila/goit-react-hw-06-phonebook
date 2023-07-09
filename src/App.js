770f85363aecb0422fc32671079c33b7add9e326
PovaliiLudmila <118770513+PovaliiLudmila@users.noreply.github.com>
2023-07-09 16:46:15 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import ContactForm from './components/ContactForm/ContactForm';
 import ContactList from './components/ContactList/ContactList';
 import Filter from './components/Filter/Filter';
-import { contactSlice } from 'redux/createSlice';
 
 const App = () => {
-  // const contacts = useSelector(state => state.contactState.contacts);
-  // const filter = useSelector(state => state.contactState.filter);
-  // const dispatch = useDispatch();
-
+ 
   return (
     <div>
       <h1>Phonebook</h1>
